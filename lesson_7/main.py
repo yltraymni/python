58e6fac126a15a5121c18aafd314077291d786d1
movavi_school <arsenti009@gmail.com>
2022-10-23 16:24:19 +0700

    23 октября

---
+++ lesson_7/main.py
@@ -0,0 +1,39 @@
+# ZeroDivisionError:
+# print(55/0)
+
+# TypeError:
+# x = 1 + "один"
+
+# IndexError:
+# lst = ["один", 2, "Антон"]
+# lst[3]
+
+# SyntaxError:
+# if 5 < 0
+
+# NameError:
+# print(x)
+
+
+# exit code:
+# 0 - все хорошо
+# 1 - ошибка
+# -1 - прерывание программы
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
