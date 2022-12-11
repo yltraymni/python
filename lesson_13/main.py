0cffd5f051be50ab60d6d217d84f7d39beae366c
movavi_school <ScallyMilano91@gamil.com>
2022-12-11 16:31:28 +0700

    11 Декабря

---
+++ lesson_13/main.py
@@ -0,0 +1,44 @@
+# a = [1,2,3,4]
+# b = [7,2,3,8]
+# index = 0
+# count = 0
+#
+# while True:
+#     if a[index] == b[index]:
+#         count += 1
+#         index += 1
+#         print(count)
+#         break
+
+# a = [0, 7, 14, 28, 0, 7, 1, 2, 3, 123]
+# b = [2, 4, 1, 78, 28, 14, 2, 43, 123]
+# a_set = set(a)
+# b_set = set(b)
+# x = list(a_set & b_set)
+# x.sort()
+# print(x)
+# lst = []
+# a = [3, 1, 4, 2, 65, 23, 2, 76, 65, 3]
+#
+# for i in a:
+#     if i in lst:
+#         print("YES")
+#     else:
+#         lst.append(i)
+#         print("NO")
+# a = [1, 2, 3, 1000000000, 123, 76, 43, 6, 9]
+# m = a[0]
+# for i in a:
+#     if i < m:
+#         m = i
+# print(m)
+lst1 = []
+lst = []
+a = [1, 7, 2, 12, 2, 45, 7]
+for i in a:
+    if i in a:
+
+
+
+
+
