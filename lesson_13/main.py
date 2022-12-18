da9909d1be1295b31bb4e47df3100bb009164462
movavi_school <klimnesterov8@mail.com>
2022-12-18 16:27:47 +0700

    18 декабря

--- lesson_13/main.py
+++ lesson_13/main.py
@@ -32,11 +32,85 @@
 #     if i < m:
 #         m = i
 # print(m)
-lst1 = []
-lst = []
-a = [1, 7, 2, 12, 2, 45, 7]
-for i in a:
-    if i in a:
+# lst1 = []
+# lst = []
+# a = [1, 7, 2, 12, 2, 45, 7]
+# for i in a:
+#     if i in a:
+
+
+# СЛОВАРЮ
+
+# dict1 = {"Ключ1": 1.2,
+#         "Ключ2 : Антон", "Якрут" : "True", "Словарь" : {}}
+
+
+# ЦИКЛЫ
+# x = 5
+# while True:
+#     print(123)
+# while False: # условие невыполнимо
+#     print("Бака")
+#
+# phrase = "фраза"
+# for i in phrase:
+#     print(i)
+#
+# for jojo in range(5):
+#     print(jojo)
+
+
+# МНОЖЕСТВА
+# set1 = dict()  # единственный способ создания пустого множества
+# set2 = {1, 2, 3, 4}  # литеральный способ
+# set = {1, 1, 1, 2}  # повторения исключены
+# print(set)
+# set0 = {"А", "Б", "Ц"}  # неупорядоченный тип данных
+# print(set0)
+# set00 = {1, [2, 3]}
+# # TypeError -> Нельзя помещать изменяемые типы данных во множество
+#
+# # Немутабельные типы данных = неизменняемые
+# # int, float, bool, str, tuple
+
+# # Оперции на множествах
+# set1 = {1, 2, 3, 4, 5,}
+# set2 = {3, 4, 5, 6, 7}
+#
+# # Объединение
+# print(set1.union(set2))
+# print(set1 | set2)
+#
+# # Пересечение
+# print(set1.intersection(set2))
+# print(set1 & set2)  # амперсанд
+#
+# # Разность
+# print(set1.difference(set2))
+# print(set1 - set2)
+#
+# # Симмертрическая разность
+# print(set1.symmetric_difference(set2))
+# print(set1 ^ set2)
+
+#
+# from random import randint
+# lst = []
+# for _ in range(5):
+#     lst.append(randint(1, 5))
+#     lst1_set = set(lst)
+#     count = len(lst1_set)
+# print(lst)
+# print(f"{count} штук: {lst1_set}")
+
+from random import randint
+
+r_start = 0
+r_end = 10_000  # 10_000 то же самое что и 10000
+size = randint(100, 1000)
+
+
+
 
 
 
