ef4768735fd0abc0b9957a26f122fb9c0d8b9147
danil <danilka.usoltsev@gmail.com>
2023-03-06 23:30:39 +0700

    проект прикол

---
+++ proekt/открывать, создавать и закрывать папки.py
@@ -0,0 +1,20 @@
+import os
+# СОЗДАНИЕ ПАПКИ
+#os.mkdir(r"c:\Users\danil\desktop\test2")
+
+# УДАЛЕНИЕ ПАПКИ
+# os.rmdir("hello")
+# os.rmdir(r"c:\Users\danil\desktop\somedir")
+
+
+# ПЕРЕИМЕНОВАНИЕ ФАЙЛА
+
+# os.rename(r"c:\Users\danil\desktop\test2\fgs.txt", r"c:\Users\danil\desktop\test2\GGRU.txt")
+f = open("file.txt", "w")
+f.write("что сучиось")
+f.close()
+# os.remove(r"c:\Users\danil\desktop\dirtest\file.txt")
+# os.rmdir(r"c:\Users\danil\desktop\dirtest")
+# os.mkdir(r"c:\Users\danil\desktop\dirtest")
+# os.replace(r"C:\Users\danil\PycharmProjects\pythondfg\proekt\file.txt", r"c:\Users\danil\desktop\dirtest\file.txt")
+os.mkdir(r"c:\Users\danil\desktop\file.txt")
