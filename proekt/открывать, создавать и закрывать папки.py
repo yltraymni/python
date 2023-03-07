aace2392a70ebe2f565595d6352557b2986bb5ca
danil <danilka.usoltsev@gmail.com>
2023-03-07 19:45:30 +0700

    проект прикол2

--- proekt/открывать, создавать и закрывать папки.py
+++ proekt/открывать, создавать и закрывать папки.py
@@ -11,10 +11,12 @@ import os
 
 # os.rename(r"c:\Users\danil\desktop\test2\fgs.txt", r"c:\Users\danil\desktop\test2\GGRU.txt")
 f = open("file.txt", "w")
-f.write("что сучиось")
+x = input()
+f.write(x)
 f.close()
-# os.remove(r"c:\Users\danil\desktop\dirtest\file.txt")
-# os.rmdir(r"c:\Users\danil\desktop\dirtest")
-# os.mkdir(r"c:\Users\danil\desktop\dirtest")
-# os.replace(r"C:\Users\danil\PycharmProjects\pythondfg\proekt\file.txt", r"c:\Users\danil\desktop\dirtest\file.txt")
-os.mkdir(r"c:\Users\danil\desktop\file.txt")
+
+#os.mkdir(r"c:\Users\danil\desktop\file.txt")
+os.remove(r"c:\Users\danil\desktop\dirtest\file.txt")
+os.rmdir(r"c:\Users\danil\desktop\dirtest")
+os.mkdir(r"c:\Users\danil\desktop\dirtest")
+os.replace(r"C:\Users\danil\PycharmProjects\pythondfg\proekt\file.txt", r"c:\Users\danil\desktop\dirtest\file.txt")
